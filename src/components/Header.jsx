156ed7667266de167b1137a99abc344c817b473c
miaelang98 <161275611+miaelang98@users.noreply.github.com>
2025-10-20 22:11:06 +0900

    page name conversion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="header">
       <div className="header-content">
-        <h1>🔍 분실물 허브</h1>
+        <h1>🔍 별가람고 분실물 허브</h1>
         <p>우리 학교 분실물을 찾아보세요</p>
       </div>
     </header>
